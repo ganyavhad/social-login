3bfc6074164698ef751dbea9c1e03e61fd45529b
ganyavhad <36654727+ganyavhad@users.noreply.github.com>
2019-11-14 23:16:31 +0530

    config set up

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+const config = require('config');
+console.log("11111111", config.get('facebook_client_id'))
 const facebook = require('./service/facebook')
 app.get('/', function (req, res) {
     res.json('Homepage')
--- service/facebook.js
+++ service/facebook.js
@@ -4,3 +4,4 @@ const router = express.Router();
 router.get('/auth', (req, res) => {
     res.json('Facebook auth')
 })
+module.exports = router;
